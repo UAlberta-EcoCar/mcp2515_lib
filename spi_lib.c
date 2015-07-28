7c0a708afd654ef8fa4ffed6523ea30bfa24abb4
Reegan McAllister <mcallisterreegan@gmail.com>
2015-07-27 19:42:10 -0600

    Create spi_lib.c

---
+++ spi_lib.c
@@ -0,0 +1,29 @@
+#include "SPI_lib.h"
+#include <avr/io.h>
+#include <avr/delay.h>
+
+#define DDR_CS DDRB
+#define PORT_CS PORTB
+#define P_CS 2
+
+#define CS_HIGH PORT_CS |= (1 << P_CS);
+
+void SPI_init(void) 
+{ 
+    // Enable pins for the SPI interface
+    DDR_SPI |= ( 1 << P_SCK ) | ( 1 << P_MOSI ) ; 
+    PORT_SPI &= ~( ( 1 << P_SCK ) | ( 1 << P_MOSI ) | ( 1 << P_MISO ) ) ;
+    DDR_CS |= ( 1 << P_CS ) ;
+    CS_HIGH 
+    // Enable the SPI master interfaces, fosc = fclk /  2 == fast :)
+    SPCR = ( 1 << SPE ) | ( 1 << MSTR );
+    SPSR = ( 1 << SPI2X ) ; 
+}
+
+unsigned char spi_putc ( unsigned char data ) 
+{ 
+    // Sends a byte
+    SPDR = data; // Waits has been sent to byte 
+    while ( ! ( SPSR & ( 1 << SPIF ) ) ); 
+    return SPDR; 
+}
