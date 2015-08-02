26ee9efc9ff74b315a7be41c524f51eb6c0c520c
Reegan McAllister <mcallisterreegan@gmail.com>
2015-08-02 15:40:03 -0600

    Update spi_lib.h

--- spi_lib.h
+++ spi_lib.h
@@ -2,17 +2,21 @@
 #define SPI_LIB_H
 
 
-
+//atmega 328p spi port is on port b
 #define DDR_SPI DDRB 
 #define PORT_SPI PORTB
+//miso is pin 4
 #define P_MISO 4 
+//mosi is pin 3
 #define P_MOSI 3 
+//sck is pin 5
 #define P_SCK 5
 
-
+//sets up spi bus
 void spi_init(void);
-unsigned char spi_putc ( unsigned char data );
 
+//sends/receives data over spi
+unsigned char spi_putc ( unsigned char data );
 
 
 #endif
