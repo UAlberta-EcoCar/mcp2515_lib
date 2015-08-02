e3bbc109752e9e4e2ec300032a260ab2c237909e
Reegan McAllister <mcallisterreegan@gmail.com>
2015-08-02 15:43:35 -0600

    Update spi_lib.cpp

--- spi_lib.cpp
+++ spi_lib.cpp
@@ -2,19 +2,15 @@
 #include <avr/io.h>
 #include <avr/delay.h>
 
-#define DDR_CS DDRB
-#define PORT_CS PORTB
-#define P_CS 2
 
-#define CS_HIGH PORT_CS |= (1 << P_CS);
-#define CS_LOW PORT_CS &= (1 << P_CS);
 void spi_init(void) 
 { 
     // Enable pins for the SPI interface
     DDR_SPI |= ( 1 << P_SCK ) | ( 1 << P_MOSI ) ; 
     PORT_SPI &= ~( ( 1 << P_SCK ) | ( 1 << P_MOSI ) | ( 1 << P_MISO ) ) ;
+    //CS pin has to be set to out put even if you're using another pin for actual CS function
     DDR_CS |= ( 1 << P_CS ) ;
-    CS_HIGH 
+    CS_HIGH
     // Enable the SPI master interfaces, fosc = fclk /  2 == fast :)
     SPCR = ( 1 << SPE ) | ( 1 << MSTR );
     SPSR = ( 1 << SPI2X ) ; 
@@ -26,5 +22,6 @@ unsigned char spi_putc ( unsigned char data )
     SPDR = data; 
     // Waits for byte to send 
     while ( ! ( SPSR & ( 1 << SPIF ) ) ); 
+    // Return received data
     return SPDR; 
 }
